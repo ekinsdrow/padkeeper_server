bafea290097d67423ef2c726adad4449ad390e21
Ivan Kolchev <ekinsdrow@gmail.com>
2022-03-28 20:55:37 +0300

    Init db

---
+++ src/main/kotlin/online/padkeeper/v1/features/auth/data/repositories/AuthRepository.kt
@@ -0,0 +1,5 @@
+package online.padkeeper.v1.features.auth.data.repositories
+
+import online.padkeeper.v1.features.auth.data.sources.AuthDatabaseClient
+
+class AuthRepository(val authDatabaseClient: AuthDatabaseClient) {}
---
+++ src/main/kotlin/online/padkeeper/v1/features/auth/data/sources/AuthDatabaseClient.kt
@@ -0,0 +1,5 @@
+package online.padkeeper.v1.features.auth.data.sources
+
+import org.jetbrains.exposed.sql.Database
+
+class AuthDatabaseClient(val database: Database) {}
